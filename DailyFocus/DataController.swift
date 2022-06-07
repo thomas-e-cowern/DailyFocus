7d6f6d2ad378d366b8ef959112f2d158377bbdbd
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-07 15:04:42 -0400

    added create sample data and preview data controller to data controller

--- DailyFocus/DataController.swift
+++ DailyFocus/DataController.swift
@@ -24,4 +24,41 @@ class DataController: ObservableObject {
             }
         }
     }
+    
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)
+        let viewContext = dataController.container.viewContext
+        
+        do {
+            try dataController.createSampleData()
+        } catch {
+            fatalError("Fatal error creating preview: \(error.localizedDescription)")
+        }
+        
+        return dataController
+        
+    }()
+    
+    func createSampleData() throws {
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            let project = Project(context: viewContext)
+            project.title = "Project \(i)"
+            project.items = []
+            project.creationDate = Date()
+            project.closed = Bool.random()
+            
+            for j in 1...10 {
+                let item = Item(context: viewContext)
+                item.title = "Item \(j)"
+                item.creationDate = Date()
+                item.completed = Bool.random()
+                item.project = project
+                item.priority = Int16.random(in: 1...3)
+            }
+        }
+        
+        try viewContext.save()
+    }
 }
