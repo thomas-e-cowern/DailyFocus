66ff0a35c47edf9128948ef8a3ae34b1f8fb9e9c
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-07 13:45:28 -0400

    initial commit

---
+++ DailyFocus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 6/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DailyFocus/DailyFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyFocusApp.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
