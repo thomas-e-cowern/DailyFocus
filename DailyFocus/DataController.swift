d9d749553f076bf7442caf31b272febd30549e95
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-07 14:48:21 -0400

    added data controller

---
+++ DailyFocus/DataController.swift
@@ -0,0 +1,15 @@
+//
+//  DataController.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 6/7/22.
+//
+
+import CoreData
+import SwiftUI
+
+class DataController: ObservableObject {
+    let container: NSPersistentCloudKitContainerOptions
+    
+    
+}
