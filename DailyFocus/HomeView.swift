fd3e8916c23b0c9a85c5f3ec9b6d3d997749a3cf
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-08 10:31:54 -0400

    added home view

---
+++ DailyFocus/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 6/8/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
