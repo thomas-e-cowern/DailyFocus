cf948bb05fa544cbf7fff1a95a959d16eac3f3e0
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-15 08:06:34 -0400

    updated project core data helper with nil coalescing and example

--- DailyFocus/Project-CoreDataHelpers.swift
+++ DailyFocus/Project-CoreDataHelpers.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+extension Project {
+    var projectTitle: String {
+        title ?? "New Project"
+    }
+    
+    var projectDetails: String {
+        detail ?? ""
+    }
+    
+    var projectColors: String {
+        color ?? "Light Blue"
+    }
+    
+    static var example: Project {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let project = Project(context: viewContext)
+        project.title = "Example Project"
+        project.detail = "This is an example project"
+        project.closed = true
+        project.creationDate = Date()
+        
+        return project
+    }
+}
