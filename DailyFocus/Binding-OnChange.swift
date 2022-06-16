369bd6048b510b67371d967b1f82fecfd3be62d9
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-16 09:46:51 -0400

    updated binding on change swift file with on change function

--- DailyFocus/Binding-OnChange.swift
+++ DailyFocus/Binding-OnChange.swift
@@ -5,4 +5,16 @@
 //  Created by Thomas Cowern New on 6/16/22.
 //
 
-import Foundation
+import SwiftUI
+
+extension Binding {
+    func onChange (_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler()
+            }
+        )
+    }
+}
