32488f0dba7215ac4d07df543732292a2f4ef2dc
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-16 08:36:12 -0400

    added init to edit item view

--- DailyFocus/EditItemView.swift
+++ DailyFocus/EditItemView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct EditItemView: View {
     
+    // MARK:  Properties
     let item: Item
     
     @EnvironmentObject var dataController: DataController
@@ -18,11 +19,23 @@ struct EditItemView: View {
     @State private var priority: Int
     @State private var completed: Bool
     
+    // MARK:  Initializer
+    init(item: Item) {
+        self.item = item
+        
+        _title = State(wrappedValue: item.itemTitle)
+        _detail = State(wrappedValue: item.itemDetail)
+        _priority = State(wrappedValue: Int(item.priority))
+        _completed = State(wrappedValue: item.completed)
+    }
+    
+    // MARK:  Body
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// MARK:  Preview
 struct EditItemView_Previews: PreviewProvider {
     static var previews: some View {
         EditItemView()
