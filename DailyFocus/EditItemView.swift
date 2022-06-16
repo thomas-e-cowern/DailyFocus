e3c2f3acfb666b62b8a3113c707045e7388de199
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-16 08:24:39 -0400

    added edit item view

---
+++ DailyFocus/EditItemView.swift
@@ -0,0 +1,20 @@
+//
+//  EditItemView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 6/16/22.
+//
+
+import SwiftUI
+
+struct EditItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditItemView()
+    }
+}
