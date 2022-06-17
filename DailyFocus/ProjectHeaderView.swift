88751173af730a889c6903920a140dcacfafb5cd
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-17 08:22:47 -0400

    added project header view

---
+++ DailyFocus/ProjectHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectHeaderView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 6/17/22.
+//
+
+import SwiftUI
+
+struct ProjectHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectHeaderView()
+    }
+}
