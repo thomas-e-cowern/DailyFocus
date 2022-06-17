4599dfa22733e09e509d33a959c9c7c58c0911c8
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-17 09:32:05 -0400

    added edit project view

---
+++ EditProjectView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProjectView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 6/17/22.
+//
+
+import SwiftUI
+
+struct EditProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProjectView()
+    }
+}
