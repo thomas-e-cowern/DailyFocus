48c7add60387787e59c82f4a9220bbc8bd9fe5d1
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-06-29 14:02:39 -0400

    added extension sequence in sequence sorting

--- DailyFocus/Sequence-Sorting.swift
+++ DailyFocus/Sequence-Sorting.swift
@@ -7,4 +7,20 @@
 
 import Foundation
 
-
+extension Sequence {
+    func sorted(by sortDescriptors: [NSSortDescriptor]) -> [Element] {
+        self.sorted {
+            for descriptor in sortDescriptors {
+                switch descriptor.compare($0, to: $1) {
+                case .orderedAscending:
+                    return true
+                case .orderedDescending:
+                    return false
+                case .orderedSame:
+                    continue
+                }
+            }
+            return false
+        }
+    }
+}
