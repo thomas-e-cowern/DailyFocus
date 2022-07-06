c3ab4efa8eb090570ce74627789170425fdca47e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-06 09:07:46 -0400

    added icon property to item row view

--- DailyFocus/ItemRowView.swift
+++ DailyFocus/ItemRowView.swift
@@ -13,6 +13,19 @@ struct ItemRowView: View {
     @ObservedObject var project: Project
     @ObservedObject var item: Item
     
+    var icon: some View {
+        if item.completed {
+            return Image(systemName: "checkmark.circle")
+                .foregroundColor(Color(project.projectColor))
+        } else if item.priority == 3 {
+            return Image(systemName: "exclamationmark.triangle")
+                .foregroundColor(Color(project.projectColor))
+        } else {
+            return Image(systemName: "checkmakr.cirlce")
+                .foregroundColor(.clear)
+        }
+    }
+    
     // MARK:  Body
     var body: some View {
         NavigationLink(destination: EditItemView(item: item)) {
