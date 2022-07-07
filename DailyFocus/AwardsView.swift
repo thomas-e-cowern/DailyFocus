17435346d521001979d26a467dc1a457ee85ae51
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-07 14:48:35 -0400

    added award view

---
+++ DailyFocus/AwardsView.swift
@@ -0,0 +1,43 @@
+//
+//  AwardsView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 7/7/22.
+//
+
+import SwiftUI
+
+struct AwardsView: View {
+    
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(Award.allAwards) { award in
+                        Button {
+                            // Coming soon
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(Color.secondary.opacity(0.5))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Awards")
+        }
+    }
+}
+
+struct AwardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwardsView()
+    }
+}
