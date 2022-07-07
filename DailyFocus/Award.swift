10edf97d3a24dbc74a520a112433082bd80960a4
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-07 14:23:02 -0400

    added awards struct and file to project

---
+++ DailyFocus/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 7/7/22.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: String
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
