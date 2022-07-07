6b4fdfa8e68172db2e7abf9ee8b7abcc720b9545
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-07 12:51:55 -0400

    added file and data checks and instantiate decoder in bundle decodable

--- DailyFocus/Bundle-Decodable.swfit.swift
+++ DailyFocus/Bundle-Decodable.swfit.swift
@@ -9,6 +9,20 @@ import Foundation
 
 extension Bundle {
     func decode<T: Decodable>(_ type: T.Type, from file: String, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyCodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
-        // more will happen here
+        
+        // Check to find the file in the bundle
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        // Load the file into a data instance
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        // Instantiate the decoder and set date and key strategies
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+        decoder.keyDecodingStrategy = keyCodingStrategy
     }
 }
