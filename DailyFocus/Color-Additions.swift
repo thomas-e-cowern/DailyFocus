7e1d5f28360ead9b251b97ea4116e232fca4f83e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-11 09:52:07 -0400

    added color additions to project

---
+++ DailyFocus/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 7/11/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
