5f907186e8160022ae5a9d8ca91e96f411db9200
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-18 15:01:28 -0400

    added item list view

---
+++ DailyFocus/ItemListView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemListView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 7/18/22.
+//
+
+import SwiftUI
+
+struct ItemListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemListView()
+    }
+}
