0423a418da6e38b1517d0b2d8bb27140a727b723
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-18 15:19:18 -0400

    added project summary view

---
+++ DailyFocus/ProjectSummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectSummaryView.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 7/18/22.
+//
+
+import SwiftUI
+
+struct ProjectSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectSummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectSummaryView()
+    }
+}
