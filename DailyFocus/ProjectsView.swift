2730526d12df97647082a9732bed484feba3cc92
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-19 09:07:57 -0400

    updated project view with add project function

--- DailyFocus/ProjectsView.swift
+++ DailyFocus/ProjectsView.swift
@@ -63,12 +63,7 @@ struct ProjectsView: View {
                         ToolbarItem(placement: .navigationBarTrailing) {
                             if showClosedProjects == false {
                                 Button {
-                                    withAnimation {
-                                        let project = Project(context: managedObjectContext)
-                                        project.closed = false
-                                        project.creationDate = Date()
-                                        dataController.save()
-                                    }
+                                    addProject()
                                 } label: {
                                     Label("Add Project", systemImage: "plus")
                                 }
@@ -115,6 +110,15 @@ struct ProjectsView: View {
         }
         
         dataController.save()
+    }
+    
+    func addProject () {
+        withAnimation {
+            let project = Project(context: managedObjectContext)
+            project.closed = false
+            project.creationDate = Date()
+            dataController.save()
+        }
     }
 }
 
