edf7a42e768759797c87a66e1baea42eb0188e5f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-19 13:06:31 -0400

    updated item core data helpers and bundle decodable with lint recommendations

--- DailyFocus/Bundle-Decodable.swfit.swift
+++ DailyFocus/Bundle-Decodable.swfit.swift
@@ -8,31 +8,34 @@
 import Foundation
 
 extension Bundle {
-    func decode<T: Decodable>(_ type: T.Type, from file: String, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyCodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
-        
+    func decode<T: Decodable>(
+        _ type: T.Type, from file: String,
+        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
+        keyCodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
+
         // Check to find the file in the bundle
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
-        
+
         // Load the file into a data instance
         guard let data = try? Data(contentsOf: url) else {
             fatalError("Failed to load \(file) from bundle.")
         }
-        
+
         // Instantiate the decoder and set date and key strategies
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = dateDecodingStrategy
         decoder.keyDecodingStrategy = keyCodingStrategy
-        
+
         do {
             return try decoder.decode(T.self, from: data)
         } catch DecodingError.keyNotFound(let key, let context) {
-            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' not found – \(context.debugDescription)")
+            fatalError("Failed to decode \(file): missing key '\(key.stringValue)' not found – \(context.debugDescription)")
         } catch DecodingError.typeMismatch(_, let context) {
-            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
+            fatalError("Failed to decode \(file): type mismatch – \(context.debugDescription)")
         } catch DecodingError.valueNotFound(let type, let context) {
-            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
+            fatalError("Failed to decode \(file): missing \(type) value – \(context.debugDescription)")
         } catch DecodingError.dataCorrupted(_) {
             fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON")
         } catch {
--- DailyFocus/Item-CoreDataHelpers.swift
+++ DailyFocus/Item-CoreDataHelpers.swift
@@ -11,19 +11,19 @@ extension Item {
     enum SortOrder {
         case optimized, title, creationDate
     }
-    
+
     var itemTitle: String {
         title ?? NSLocalizedString("New Item", comment: "Create a new item")
     }
-    
+
     var itemDetail: String {
         detail ?? ""
     }
-    
+
     var itemCreationDate: Date {
         creationDate ?? Date()
     }
-    
+
     static var example: Item {
         let controller = DataController(inMemory: true)
         let viewContext = controller.container.viewContext
@@ -33,7 +33,7 @@ extension Item {
         item.detail = "This is an example item"
         item.priority = 3
         item.creationDate = Date()
-        
+
         return item
     }
 }
