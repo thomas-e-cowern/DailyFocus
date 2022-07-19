452891cfcfc7b6782503a5740dc2f6dd390b7b74
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-19 10:49:16 -0400

    updated project core data helpers with linting recommendations

--- DailyFocus/Project-CoreDataHelpers.swift
+++ DailyFocus/Project-CoreDataHelpers.swift
@@ -9,27 +9,40 @@ import Foundation
 import SwiftUI
 
 extension Project {
-    
-    static let colors = ["Pink", "Purple", "Red", "Orange", "Gold", "Green", "Teal", "Light Blue", "Dark Blue", "Midnight", "Dark Gray", "Gray"]
-    
+
+    static let colors = [
+        "Pink",
+        "Purple",
+        "Red",
+         "Orange",
+         "Gold",
+         "Green",
+         "Teal",
+         "Light Blue",
+         "Dark Blue",
+         "Midnight",
+         "Dark Gray",
+         "Gray"
+    ]
+
     var projectTitle: String {
         title ?? NSLocalizedString("New Project", comment: "Create a new project")
     }
-    
+
     var projectDetails: String {
         detail ?? ""
     }
-    
+
     var projectColor: String {
         color ?? "Light Blue"
     }
-    
+
     var projectItems: [Item] {
         items?.allObjects as? [Item] ?? []
     }
-    
+
     var projectItemsDefaultSorted: [Item] {
-        
+
         projectItems.sorted { first, second in
             if first.completed == false {
                 if second.completed == true {
@@ -40,17 +53,17 @@ extension Project {
                     return false
                 }
             }
-            
+
             if first.priority > second.priority {
                 return true
             } else if first.priority < second.priority {
                 return false
             }
-            
+
             return first.itemCreationDate < second.itemCreationDate
         }
     }
-    
+
     var completionAmount: Double {
         let originalItems = items?.allObjects as? [Item] ?? []
         guard originalItems.isEmpty == false else { return 0 }
@@ -58,24 +71,24 @@ extension Project {
         let completedItems = originalItems.filter(\.completed)
         return Double(completedItems.count) / Double(originalItems.count)
     }
-    
+
     var label: LocalizedStringKey {
         LocalizedStringKey("\(projectTitle), \(projectItems.count) items, \(completionAmount * 100, specifier: "%g")% complete")
     }
-    
+
     static var example: Project {
         let controller = DataController(inMemory: true)
         let viewContext = controller.container.viewContext
-        
+
         let project = Project(context: viewContext)
         project.title = "Example Project"
         project.detail = "This is an example project"
         project.closed = true
         project.creationDate = Date()
-        
+
         return project
     }
-    
+
     func projectItems(using sortOrder: Item.SortOrder) -> [Item] {
         switch sortOrder {
         case .optimized:
