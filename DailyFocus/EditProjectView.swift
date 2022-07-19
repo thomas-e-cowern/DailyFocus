a11eb6e35ff01342fbbbc731adf66f53882e0b0a
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-19 12:51:58 -0400

    updated edit project view with lint recommendations

--- DailyFocus/EditProjectView.swift
+++ DailyFocus/EditProjectView.swift
@@ -8,29 +8,29 @@
 import SwiftUI
 
 struct EditProjectView: View {
-    
+
     let project: Project
-    
+
     @EnvironmentObject var dataController: DataController
     @Environment(\.presentationMode) var presentationMode
     @State private var showingDeleteConfirm = false
-    
+
     @State private var title: String
     @State private var detail: String
     @State private var color: String
-    
+
     let colorColumns = [
         GridItem(.adaptive(minimum: 44))
     ]
-    
+
     init (project: Project) {
         self.project = project
-        
+
         _title = State(wrappedValue: project.projectTitle)
         _detail = State(wrappedValue: project.projectDetails)
         _color = State(wrappedValue: project.projectColor)
     }
-    
+
     var body: some View {
         Form {
             // Section 1
@@ -51,7 +51,7 @@ struct EditProjectView: View {
                     project.closed.toggle()
                     update()
                 }
-                
+
                 Button("Delete this project") {
                     showingDeleteConfirm.toggle()
                 }
@@ -64,26 +64,26 @@ struct EditProjectView: View {
             Alert(title: Text("Delete project?"), message: Text("Are you sure you want to delete this project?  You will also delete all the items it contains."), primaryButton: .default(Text("Delete"), action: delete), secondaryButton: .cancel())
         }
     }
-    
-    
+
+
     // MARK:  Methods
     func update () {
         project.title = title
         project.detail = detail
         project .color = color
     }
-    
+
     func delete () {
         dataController.delete(project)
         presentationMode.wrappedValue.dismiss()
     }
-    
+
     func colorButton(for item: String) -> some View {
         ZStack {
             Color(item)
                 .aspectRatio(contentMode: .fit)
                 .cornerRadius(6)
-            
+
             if item == color {
                 Image(systemName: "checkmark.circle")
                     .foregroundColor(.white)
@@ -96,7 +96,7 @@ struct EditProjectView: View {
         }
         .accessibilityElement(children: .ignore)
         .accessibilityAddTraits(
-        
+
             item == color ? [.isButton, .isSelected] : .isButton
         )
         .accessibilityLabel(LocalizedStringKey(item))
