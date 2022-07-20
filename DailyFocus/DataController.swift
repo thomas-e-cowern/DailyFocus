2f661e3dcf30c5ef05f8dbac648c7decf50a5687
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-20 02:56:25 -0400

    added additional comments to data controller

--- DailyFocus/DataController.swift
+++ DailyFocus/DataController.swift
@@ -8,11 +8,19 @@
 import CoreData
 import SwiftUI
 
+/// An environment singleton responsible for managing our Core Data stack, including handling saving,
+/// counting fetch requests, tracking awards, and dealing with sample data.
 class DataController: ObservableObject {
+
+    /// The lone cloudkit container used to store all our data
     let container: NSPersistentCloudKitContainer
 
+    /// Initializes a data controller, either in memory (for temporary use such as testing and previewing),
+    /// or on permanent storage (for use in regular app runs.) Defaults to permanent storage.
+    /// - Parameter inMemory: whether to stroe this data in temporary memory or not.
     init(inMemory: Bool = false) {
         print("Data Controller Init")
+
         container = NSPersistentCloudKitContainer(name: "Main")
 
         // For testing and previewing purposes, we create a
@@ -45,6 +53,7 @@ class DataController: ObservableObject {
     }()
 
     // Creating sample data
+    /// - Throws: An NSError sent from calling save() on the NSManagedObjectContext
     func createSampleData() throws {
         let viewContext = container.viewContext
 
@@ -68,7 +77,8 @@ class DataController: ObservableObject {
         try? viewContext.save()
     }
 
-    // Save after checking for changes
+    /// Saves our Core Data context iff there are changes. This silently ignores
+    /// any errors caused by saving, but this should be fine because all our attributes are optional.
     func save () {
         if container.viewContext.hasChanges {
             try? container.viewContext.save()
