cb35258999731b74bab2b257f9bdcf45bfba8d50
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-20 02:40:51 -0400

    added comments to data controller.

--- DailyFocus/DataController.swift
+++ DailyFocus/DataController.swift
@@ -15,6 +15,9 @@ class DataController: ObservableObject {
         print("Data Controller Init")
         container = NSPersistentCloudKitContainer(name: "Main")
 
+        // For testing and previewing purposes, we create a
+        // temporary, in-memory database by writing to /dev/null
+        // so our data is destroyed after the app finishes running
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
         }
@@ -95,11 +98,14 @@ class DataController: ObservableObject {
 
     func hasEarned(award: Award) -> Bool {
         switch award.criterion {
+
+        // returns true if they added a certain number of items
         case "items":
             let fetchRequest: NSFetchRequest<Item> = NSFetchRequest(entityName: "Item")
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
 
+        // returns true if they completed a certain number of items
         case "complete":
             let fetchRequest: NSFetchRequest<Item> = NSFetchRequest(entityName: "Item")
             fetchRequest.predicate = NSPredicate(format: "completed = true")
@@ -107,7 +113,7 @@ class DataController: ObservableObject {
             return awardCount >= award.value
 
         default:
-            // fatalError("Unknown award criterion \(award.criterion)")
+            // an unknown award criterion; this should never be allowed
             return false
         }
     }
