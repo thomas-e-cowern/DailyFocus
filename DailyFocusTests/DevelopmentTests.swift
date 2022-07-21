6b7a6ec10f7b50549ba93ba63504c5160d3433a9
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-21 09:22:25 -0400

    added test sample data creation works to development tests

--- DailyFocusTests/DevelopmentTests.swift
+++ DailyFocusTests/DevelopmentTests.swift
@@ -6,30 +6,15 @@
 //
 
 import XCTest
+import CoreData
+@testable import DailyFocus
 
 class DevelopmentTests: BaseTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testSampleDataCreationWorks() throws {
+        try dataController.createSampleData()
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 5, "There should be 5 sample projects.")
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 50, "There should be 50 sample items.")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
