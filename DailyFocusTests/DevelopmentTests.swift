4adae6ef40ff04c45e2494df8b5ffd22d417f543
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-21 18:54:46 -0400

    added text example project closed and example item is high priority to development tests

--- DailyFocusTests/DevelopmentTests.swift
+++ DailyFocusTests/DevelopmentTests.swift
@@ -17,4 +17,14 @@ class DevelopmentTests: BaseTestCase {
         XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 5, "There should be 5 sample projects.")
         XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 50, "There should be 50 sample items.")
     }
+
+    func testExampleProjectIsClosed() {
+        let project = Project.example
+        XCTAssertTrue(project.closed, "The example project should be closed.")
+    }
+
+    func testExampleItemIsHighPriority() {
+        let item = Item.example
+        XCTAssertEqual(item.priority, 3, "The example item should be high priority.")
+    }
 }
