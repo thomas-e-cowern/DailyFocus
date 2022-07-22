e2d1b67bca5bf1e795deae2264b813ad5a35e9d9
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-22 11:51:36 -0400

    added test sequence key path sorting self to extensiont ests

--- DailyFocusTests/ExtensionTests.swift
+++ DailyFocusTests/ExtensionTests.swift
@@ -10,27 +10,9 @@ import XCTest
 
 class ExtensionTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testSequenceKeyPathSortingSelf() {
+        let items = [1, 4, 5, 3, 2]
+        let sortedItems = items.sorted(by: \.self)
+        XCTAssertEqual(sortedItems, [1, 2, 3, 4, 5], "The sorted numbers must be ascending")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
