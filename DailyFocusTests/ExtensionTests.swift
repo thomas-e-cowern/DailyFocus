850981804f9262c52112b150791290d37951e3ab
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-26 06:53:36 -0400

    added test decoding string to extension tests

--- DailyFocusTests/ExtensionTests.swift
+++ DailyFocusTests/ExtensionTests.swift
@@ -9,9 +9,17 @@ import XCTest
 @testable import DailyFocus
 
 class ExtensionTests: XCTestCase {
+    
+    let bundle = Bundle(for: ExtensionTests.self)
 
     func testBundleDecodingAwards() {
         let awards = Bundle.main.decode([Award].self, from: "Awards.json")
         XCTAssertFalse(awards.isEmpty, "Awards.json should decode to a non-empty array.")
+    }
+
+    func testDecodingString() {
+        let bundle = Bundle(for: ExtensionTests.self)
+        let data = bundle.decode(String.self, from: "DecodableString.json")
+        XCTAssertEqual(data, "The rain in Spain falls mainly on the Spaniards.", "The string must match the content of DecodableString.json.")
     }
 }
