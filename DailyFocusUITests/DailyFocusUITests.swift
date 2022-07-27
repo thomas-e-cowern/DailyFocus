66407b031880d9d8e514e4d4e5342b5f362f8a0f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-27 17:30:45 -0400

    updated ui tests

--- DailyFocusUITests/DailyFocusUITests.swift
+++ DailyFocusUITests/DailyFocusUITests.swift
@@ -9,21 +9,22 @@ import XCTest
 
 class DailyFocusUITests: XCTestCase {
 
+    var app: XCUIApplication!
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let app = XCUIApplication()
+        app.launchArguments = ["enable-testing"]
+        app.launch()
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        continueAfterFailure = true
 
         // In UI tests it’s important to set the initial state - such as interface orientation -
         // required for your tests before they run. The setUp method is a good place to do this.
     }
 
     func testAppHas4Tabs() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launchArguments = ["enable-testing"]
-        app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual(app.tabBars.buttons.count, 4, "There should be 4 tabs in the app")
