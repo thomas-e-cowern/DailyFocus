6e5d29dbe294840391ebfdacf35f9abc15913d10
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-27 16:04:18 -0400

    added performance tests to daily focus tests

---
+++ DailyFocusTests/PerformanceTests.swift
@@ -0,0 +1,16 @@
+//
+//  PerformanceTests.swift
+//  DailyFocusTests
+//
+//  Created by Thomas Cowern New on 7/27/22.
+//
+
+import XCTest
+import CoreData
+import DailyFocus
+
+class PerformanceTests: BaseTestCase {
+
+
+
+}
