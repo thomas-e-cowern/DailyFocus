07cfdaee164f940d411b5ca79c5b985a96a39661
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-28 17:51:28 -0400

    added test editing project updates correctly to daily focus ui test

--- DailyFocusUITests/DailyFocusUITests.swift
+++ DailyFocusUITests/DailyFocusUITests.swift
@@ -48,15 +48,45 @@ class DailyFocusUITests: XCTestCase {
             XCTAssertEqual(app.tables.cells.count, tapCount, "There should be \(tapCount) rows(s) in the list.")
         }
     }
-    
+
     func testAddingItemInsertsRows() {
+
+        let app = XCUIApplication()
+        app.launchArguments = ["enable-testing"]
+        app.launch()
+
         app.buttons["Open"].tap()
         XCTAssertEqual(app.tables.cells.count, 0, "There should be no list rows initially.")
 
-        app.buttons["add"].tap()
+        app.buttons["Add Project"].tap()
         XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 list row after adding a project.")
 
         app.buttons["Add New Item"].tap()
         XCTAssertEqual(app.tables.cells.count, 2, "There should be 2 list rows after adding an item.")
+    }
+
+    func testEditingProjectUpdatesCorrectly() {
+
+        let app = XCUIApplication()
+        app.launchArguments = ["enable-testing"]
+        app.launch()
+
+        app.buttons["Open"].tap()
+        XCTAssertEqual(app.tables.cells.count, 0, "There should be no list rows initially.")
+
+        app.buttons["Add Project"].tap()
+        XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 list row after adding a project.")
+
+        app.buttons["NEW PROJECT"].tap()
+        app.textFields["Project name"].tap()
+
+        app.keys["space"].tap()
+        app.keys["more"].tap()
+        app.keys["2"].tap()
+        app.buttons["Return"].tap()
+
+        app.buttons["Open Projects"].tap()
+
+        XCTAssertTrue(app.buttons["NEW PROJECT 2"].exists, "The new project name should be visible in the list.")
     }
 }
