fb07100b92f858f80d1e1fd9a3e51e5cb7c0e387
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-07-28 21:34:40 -0400

    added test editing item updates correctly

--- DailyFocusUITests/DailyFocusUITests.swift
+++ DailyFocusUITests/DailyFocusUITests.swift
@@ -89,4 +89,32 @@ class DailyFocusUITests: XCTestCase {
 
         XCTAssertTrue(app.buttons["NEW PROJECT 2"].exists, "The new project name should be visible in the list.")
     }
+    
+    func testEditingItemUpdatesCorrectly() {
+
+        let app = XCUIApplication()
+        app.launchArguments = ["enable-testing"]
+        app.launch()
+
+        app.buttons["Open"].tap()
+        XCTAssertEqual(app.tables.cells.count, 0, "There should be no list rows initially.")
+
+        app.buttons["add"].tap()
+        XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 list row after adding a project.")
+
+        app.buttons["Add New Item"].tap()
+        XCTAssertEqual(app.tables.cells.count, 2, "There should be 2 list rows after adding an item.")
+
+        app.buttons["New Item"].tap()
+
+        app.textFields["Item name"].tap()
+        app.keys["space"].tap()
+        app.keys["more"].tap()
+        app.keys["2"].tap()
+        app.buttons["Return"].tap()
+
+        app.buttons["Open Projects"].tap()
+
+        XCTAssertTrue(app.buttons["New Item 2"].exists, "The new item name should be visible in the list.")
+    }
 }
