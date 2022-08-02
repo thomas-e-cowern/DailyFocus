7e4965eed99f4ddd49e6ce03d983446bba775e8c
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-02 09:08:22 -0400

    added data controller and projects fetch request to home view model

--- DailyFocus/HomeViewModel.swift
+++ DailyFocus/HomeViewModel.swift
@@ -12,5 +12,26 @@ extension HomeView {
     class ViewModel: NSObject, ObservableObject, NSFetchedResultsControllerDelegate {
         private let projectsController: NSFetchedResultsController<Project>
         private let itemsController: NSFetchedResultsController<Item>
+        
+        @Published var projects = [Project]()
+        @Published var items = [Item]()
+        
+        var dataController: DataController
+        
+        init(dataController: DataController) {
+            self.dataController = dataController
+            
+            // Construct a fetch request to show all open projects
+            let projectRequest: NSFetchRequest<Project> = Project.fetchRequest()
+            projectRequest.predicate = NSPredicate(format: "closed = false")
+            projectRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Project.title, ascending: true)]
+            
+            projectsController = NSFetchedResultsController(
+                fetchRequest: projectRequest,
+                managedObjectContext: dataController.container.viewContext,
+                sectionNameKeyPath: nil,
+                cacheName: nil
+            )
+        }
     }
 }
