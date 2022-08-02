40d70f40425dc68d2e9bee3ca606c52b7b38e021
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-02 09:25:38 -0400

    added items fetch request to home view modek

--- DailyFocus/HomeViewModel.swift
+++ DailyFocus/HomeViewModel.swift
@@ -12,22 +12,40 @@ extension HomeView {
     class ViewModel: NSObject, ObservableObject, NSFetchedResultsControllerDelegate {
         private let projectsController: NSFetchedResultsController<Project>
         private let itemsController: NSFetchedResultsController<Item>
-        
+
         @Published var projects = [Project]()
         @Published var items = [Item]()
-        
+
         var dataController: DataController
-        
+
         init(dataController: DataController) {
             self.dataController = dataController
-            
+
             // Construct a fetch request to show all open projects
             let projectRequest: NSFetchRequest<Project> = Project.fetchRequest()
             projectRequest.predicate = NSPredicate(format: "closed = false")
             projectRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Project.title, ascending: true)]
-            
+
             projectsController = NSFetchedResultsController(
                 fetchRequest: projectRequest,
+                managedObjectContext: dataController.container.viewContext,
+                sectionNameKeyPath: nil,
+                cacheName: nil
+            )
+
+            // Construct a fetch request to show 10 highest priority items if still open
+            let itemRequest: NSFetchRequest<Item> = Item.fetchRequest()
+            let completedPredicate = NSPredicate(format: "closed = false")
+            let openPredicate = NSPredicate(format: "project.closed = false")
+            let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: [completedPredicate, openPredicate])
+            itemRequest.predicate = compoundPredicate
+
+            itemRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Item.priority, ascending: false)]
+
+            itemRequest.fetchLimit = 10
+            
+            itemsController = NSFetchedResultsController(
+                fetchRequest: itemRequest,
                 managedObjectContext: dataController.container.viewContext,
                 sectionNameKeyPath: nil,
                 cacheName: nil
