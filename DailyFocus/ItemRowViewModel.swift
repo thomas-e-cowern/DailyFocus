2a228d4b7a69409d512e1e33b522fe503799fdcc
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-02 11:26:59 -0400

    added icon and label to item row view model

--- DailyFocus/ItemRowViewModel.swift
+++ DailyFocus/ItemRowViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+extension ItemRowView {
+    class ViewModel: ObservableObject {
+
+        let project: Project
+        let ite: Item
+        
+        var icon: some View {
+            if item.completed {
+                return Image(systemName: "checkmark.circle")
+                    .foregroundColor(Color(project.projectColor))
+            } else if item.priority == 3 {
+                return Image(systemName: "exclamationmark.triangle")
+                    .foregroundColor(Color(project.projectColor))
+            } else {
+                return Image(systemName: "checkmark.circle")
+                    .foregroundColor(.clear)
+            }
+        }
+
+        var label: Text {
+            if item.completed {
+                return Text("\(item.itemTitle), completed")
+            } else if item.priority == 3 {
+                return Text("\(item.itemTitle), high priority")
+            } else {
+                return Text(item.itemTitle)
+            }
+        }
+
+        internal init(project: Project, ite: Item) {
+            self.project = project
+            self.ite = ite
+        }
+    }
+}
