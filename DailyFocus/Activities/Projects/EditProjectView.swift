8cbfd795006ee60ea0017b4ac6ef0c7713856f9c
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-03 12:07:22 -0400

    added events, pattern and player for haptics to edit project view

--- DailyFocus/Activities/Projects/EditProjectView.swift
+++ DailyFocus/Activities/Projects/EditProjectView.swift
@@ -88,7 +88,29 @@ struct EditProjectView: View {
                 let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
                 let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
 
-                let parameter = CHHapticParameterCurve(parameterID: .hapticIntensityControl, controlPoints: [start, end], relativeTime: 0)
+                let parameter = CHHapticParameterCurve(
+                    parameterID: .hapticIntensityControl,
+                    controlPoints: [start, end],
+                    relativeTime: 0
+                )
+
+                let event1 = CHHapticEvent(
+                    eventType: .hapticTransient,
+                    parameters: [intensity, sharpness],
+                    relativeTime: 0
+                )
+                let event2 = CHHapticEvent(
+                    eventType: .hapticContinuous,
+                    parameters: [sharpness, intensity],
+                    relativeTime: 0.125,
+                    duration: 1
+                )
+
+                let pattern = try CHHapticPattern(events: [event1, event2], parameterCurves: [parameter])
+
+                let player = try engine?.makePlayer(with: pattern)
+                try player?.stop(atTime: 0)
+
             } catch {
                 // Haptics didn't work
                 print("Haptics failed to work")
