45292c16d511bb09f0778a489cb21ca6d1d2ecce
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-09 10:36:26 -0400

    updated scene delegate with window scene method

--- DailyFocus/SceneDelegate.swift
+++ DailyFocus/SceneDelegate.swift
@@ -9,5 +9,17 @@ import Foundation
 import SwiftUI
 
 class SceneDelegate: NSObject, UIWindowSceneDelegate {
-    
+    @Environment(\.openURL) var openUrl
+
+    func windowScene(
+        _ windowScene: UIWindowScene,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void) {
+        guard let url = URL(string: shortcutItem.type) else {
+            completionHandler(false)
+            return
+        }
+
+        openUrl(url, completion: completionHandler)
+    }
 }
