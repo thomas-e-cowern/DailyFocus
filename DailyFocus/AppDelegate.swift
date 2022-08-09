8cf5ce43352b913b4739100834bd7b53686aced8
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-09 10:16:05 -0400

    added app delegate and scene delegate to project

---
+++ DailyFocus/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 8/9/22.
+//
+
+import Foundation
---
+++ DailyFocus/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  DailyFocus
+//
+//  Created by Thomas Cowern New on 8/9/22.
+//
+
+import Foundation
