39f1a04959fd5e399a0e258f4ccb3473adc653c6
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-09 10:23:43 -0400

    updated app delegate with application method

--- DailyFocus/AppDelegate.swift
+++ DailyFocus/AppDelegate.swift
@@ -9,5 +9,12 @@ import Foundation
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
+    }
 }
