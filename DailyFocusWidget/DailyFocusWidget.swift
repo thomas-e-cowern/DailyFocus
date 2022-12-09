1000067154435d1cc3e8556aa144f92f6bfcb3d3
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-09 02:28:07 -0500

    add DailyFocusWidget

---
+++ DailyFocusWidget/DailyFocusWidget.swift
@@ -0,0 +1,66 @@
+//
+//  DailyFocusWidget.swift
+//  DailyFocusWidget
+//
+//  Created by Thomas Cowern on 12/9/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct DailyFocusWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+struct DailyFocusWidget: Widget {
+    let kind: String = "DailyFocusWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            DailyFocusWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct DailyFocusWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyFocusWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ DailyFocusWidget/DailyFocusWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DailyFocusWidgetBundle.swift
+//  DailyFocusWidget
+//
+//  Created by Thomas Cowern on 12/9/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DailyFocusWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DailyFocusWidget()
+        DailyFocusWidgetLiveActivity()
+    }
+}
