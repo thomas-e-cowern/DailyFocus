d343697294e018414c492f5cefc17b7c1daa37ae
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-14 18:20:36 -0500

    updateed

--- DailyFocusWidget/DailyFocusWidget.swift
+++ DailyFocusWidget/DailyFocusWidget.swift
@@ -8,37 +8,37 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), items: [Item.example])
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), items: loadItems())
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        let entry = SimpleEntry(date: Date(), items: loadItems())
-        
-        let timeline = Timeline(entries: [entry], policy: .never)
-        completion(timeline)
-    }
-    
-    func loadItems() -> [Item] {
-        let dataController = DataController()
-        let itemRequest = dataController.fetchRequestForTopItems(count: 5)
-        return dataController.results(for: itemRequest)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let items: [Item]
-}
+//struct Provider: TimelineProvider {
+//    func placeholder(in context: Context) -> SimpleEntry {
+//        SimpleEntry(date: Date(), items: [Item.example])
+//    }
+//
+//    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+//        let entry = SimpleEntry(date: Date(), items: loadItems())
+//        completion(entry)
+//    }
+//
+//    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+//        let entry = SimpleEntry(date: Date(), items: loadItems())
+//        
+//        let timeline = Timeline(entries: [entry], policy: .never)
+//        completion(timeline)
+//    }
+//    
+//    func loadItems() -> [Item] {
+//        let dataController = DataController()
+//        let itemRequest = dataController.fetchRequestForTopItems(count: 5)
+//        return dataController.results(for: itemRequest)
+//    }
+//}
+//
+//struct SimpleEntry: TimelineEntry {
+//    let date: Date
+//    let items: [Item]
+//}
 
 struct DailyFocusWidgetEntryView : View {
-    var entry: Provider.Entry
+    var entry: Provider.entry
 
     var body: some View {
         VStack {
