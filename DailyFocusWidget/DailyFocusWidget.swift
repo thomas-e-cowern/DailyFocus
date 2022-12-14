fe433ee1cc19646f50bbe613b955bff3261629cf
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-14 17:36:32 -0500

    add ComplesDailyFocusWidget to DailyFocusWidget

--- DailyFocusWidget/DailyFocusWidget.swift
+++ DailyFocusWidget/DailyFocusWidget.swift
@@ -75,6 +75,18 @@ struct DailyFocusWidgetMultipleEntryView: View {
     }
 }
 
+struct ComplexDailyFocusWidget: Widget {
+    let kind: String = "ComplexDailyFocusWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            DailyFocusWidgetMultipleEntryView(entry: entry)
+        }
+        .configurationDisplayName("Up next…")
+        .description("Your most important items.")
+    }
+}
+
 struct DailyFocusWidget_Previews: PreviewProvider {
     static var previews: some View {
         DailyFocusWidgetEntryView(entry: SimpleEntry(date: Date(), items: [Item.example]))
