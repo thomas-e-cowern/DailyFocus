cfcb48bf8bc901844cab7230a40e8c1990b7a79d
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-15 06:21:17 -0500

    update widget bundle

--- DailyFocusWidget/DailyFocusWidgetBundle.swift
+++ DailyFocusWidget/DailyFocusWidgetBundle.swift
@@ -11,7 +11,7 @@ import SwiftUI
 @main
 struct DaikyFocusWidgets: WidgetBundle {
     var body: some Widget {
-        SimpleDailyFocusWidget()
+//        SimpleDailyFocusWidget()
         ComplexDailyFocusWidget()
     }
 }
