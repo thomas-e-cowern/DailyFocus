54f0d1a56044305dfbe756de922a13af0fc4863e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-15 06:31:11 -0500

    update DataController+StoreKit

--- DailyFocus/DataController-StoreKit.swift
+++ DailyFocus/DataController-StoreKit.swift
@@ -5,4 +5,17 @@
 //  Created by Thomas Cowern on 12/15/22.
 //
 
-import Foundation
+import StoreKit
+
+extension DataController {
+    func appLaunched() {
+        guard count(for: Project.fetchRequest()) >= 5 else { return }
+
+        let allScenes = UIApplication.shared.connectedScenes
+        let scene = allScenes.first { $0.activationState == .foregroundActive }
+
+        if let windowScene = scene as? UIWindowScene {
+            SKStoreReviewController.requestReview(in: windowScene)
+        }
+    }
+}
